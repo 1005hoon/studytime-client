d5ce96efa747d96acbbc61b1502a82cee82fea54
Hoon Oh <shoh@help-me.kr>
2021-12-29 17:59:34 +0900

    feat(User): 사용자 조회결과 유저 리스트 콤포넌트에 등록

--- src/container/users/UsersList.tsx
+++ src/container/users/UsersList.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect } from 'react';
 import { useActions } from '../../hooks/use-actions';
+import { useTypedSelector } from '../../hooks/use-typed-selector';
 
 interface UsersListProps {}
 
 const UsersList: React.FC<UsersListProps> = (props) => {
   const { onFetchAllUsers } = useActions();
-
+  const { loading, data, error } = useTypedSelector((state) => state.userList);
   useEffect(() => {
     onFetchAllUsers();
   }, []);
