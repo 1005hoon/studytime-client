ca4af8b5b98cb6592bcaa2a7b05ade207737ba7c
Hoon Oh <shoh@help-me.kr>
2021-12-30 10:04:39 +0900

    chore: 서버 연결 끊겼을때 에러메시지 처리

--- src/utils/axios-error.handler.ts
+++ src/utils/axios-error.handler.ts
@@ -5,14 +5,18 @@ export const axiosErrorHandler = (e: AxiosError) => {
 
   if (e.response) {
     // Request made and server responded
-
     message = e.response.data.message;
   } else if (e.request) {
     // The request was made but no response was received
     message = e.request;
+
+    if (e.request.status === 0) {
+      message = '서버연결 오류';
+    }
   } else {
     // Something happened in setting up the request that triggered an Error
     message = e.message;
   }
+
   return message;
 };
