7bff0b24adfb56b738ea1ec352d7a120ac737eaa
Hoon Oh <shoh@help-me.kr>
2022-01-03 21:41:12 +0900

    feat(GoogleAuth): 글로벌 네임스페이스에 process.env 환경변수 등록

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,16 @@
 /// <reference types="react-scripts" />
+
+namespace NodeJS {
+  interface ProcessEnv {
+    REACT_APP_SERVER_URL: string;
+    REACT_APP_NODE_ENV: string;
+
+    REACT_APP_OAUTH_KAKAO_BASE_HOST: string;
+    REACT_APP_OAUTH_KAKAO_CLIENT_ID: string;
+    REACT_APP_OAUTH_KAKAO_CLIENT_SECRET: string;
+    REACT_APP_OAUTH_KAKAO_REDIRECT_URI: string;
+
+    REACT_APP_GOOGLE_OAUTH_CLIENT_ID: string;
+    REACT_APP_OAUTH_GOOGLE_REDIRECT_URI: string;
+  }
+}
