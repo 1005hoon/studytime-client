82b22e4e56c751d6154cdea1274e7957f73ce565
Hoon Oh <shoh@help-me.kr>
2022-01-04 14:03:06 +0900

    refactor(layout): 페이지 헤더 사이즈 수정

--- src/components/page-header/styles.ts
+++ src/components/page-header/styles.ts
@@ -2,19 +2,19 @@ import styled from 'styled-components/macro';
 
 export const StyledPageHeader = styled.header`
   height: 86px;
+  width: 100%;
   border-bottom: 5px solid #24262b;
   display: flex;
   align-items: center;
   justify-content: space-between;
-  width: 100%;
+  padding: 20px 50px;
 `;
 
 export const StyledPageHeaderContainer = styled.div`
-  margin: 0 auto;
   max-width: 1400px;
   min-width: 1100px;
   width: 100%;
-  height: 100%;
+  margin: 0 auto;
   display: flex;
   justify-content: space-between;
   align-items: center;
